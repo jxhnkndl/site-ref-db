4d04b3687c55389c3c753cab85d412b1e4c5d0b9
jxhnkndl <jkroyston@gmail.com>
2023-10-23 23:08:57 -0400

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>
+          <span className={styles.echo}>echo</span>DB
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+const styles = {
+  header: 'py-4 mb-6 text-2xl text-light bg-dark',
+
+  container: 'w-[90%] mx-auto flex justify-between items-center',
+
+  echo: 'font-normal',
+
+  h1: 'font-bold',
+};
