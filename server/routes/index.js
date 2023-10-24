07090c549121f6b28a3666f7191d849ef2c71e61
jxhnkndl <jkroyston@gmail.com>
2023-10-23 21:36:08 -0400

    fix incorrect import

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const apiRoutes = require('./api/index');
 
-router.use('/api', siteRoutes);
+router.use('/api', apiRoutes);
 
 // Serve React app to client in production
 router.use((req, res) => {
