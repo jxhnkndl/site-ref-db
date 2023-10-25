27da5ea07321f01dd6c5ffd5ed9bcd0013a26057
jxhnkndl <jkroyston@gmail.com>
2023-10-25 19:37:05 -0400

    add timestamps to site documents and return all sites ordered by date created

--- server/controllers/site-controller.js
+++ server/controllers/site-controller.js
@@ -3,7 +3,8 @@ const { Site } = require('../models');
 module.exports = {
   async getAllSites(req, res) {
     try {
-      const allSites = await Site.find({});
+      // Return sites sorted with newest additions at the top
+      const allSites = await Site.find({}).sort({ createdAt: -1 });
       res.status(200).json(allSites);
     } catch (error) {
       console.error(error);
@@ -12,8 +13,11 @@ module.exports = {
   },
 
   async createSite(req, res) {
+    console.log(123);
     try {
       const newSite = await Site.create(req.body);
+
+      console.log(newSite)
 
       if (!newSite) {
         return res.status(400).json({ msg: 'Something went wrong' });
--- server/models/Site.js
+++ server/models/Site.js
@@ -1,24 +1,27 @@
 const { Schema, model } = require('mongoose');
 
-const siteSchema = new Schema({
-  url: {
-    type: String,
-    required: [true, 'URL is required'],
-    unique: true,
-    trim: true,
-  },
-  title: {
-    type: String,
-    required: [true, 'Title is required'],
-    trim: true,
-  },
-  keywords: [
-    {
-      keyword: { type: String },
-      name: { type: String },
+const siteSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: [true, 'URL is required'],
+      unique: true,
+      trim: true,
     },
-  ],
-});
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true,
+    },
+    keywords: [
+      {
+        keyword: { type: String },
+        name: { type: String },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 // Check to see if model has already been built before rebuilding it
 const Site = model('Site', siteSchema);
