cd99b38ea9753af07ed44b655a82a48b2abf9101
jxhnkndl <jkroyston@gmail.com>
2023-10-25 15:54:42 -0400

    set up routes for results and creating new sites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import CreateForm from './components/CreateForm';
-import Site from './components/Site';
+import Results from './pages/Results';
+import CreateForm from './pages/CreateForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -20,19 +21,18 @@ export default function App() {
   }, []);
 
   return (
-    <div className={`${styles.pageContainer}`}>
-      <Header />
-      <main className={`${styles.main}`}>
-        <aside className={`${styles.sidebar}`}>
-          <CreateForm />
-        </aside>
-        <section className={`${styles.results}`}>
-          <h2 className={`${styles.h2}`}>Results</h2>
-          {sites && sites.map((site) => <Site key={site._id} site={site} />)}
-        </section>
-      </main>
-      <ToastContainer autoClose={3000} />
-    </div>
+    <Router>
+      <div className={`${styles.pageContainer}`}>
+        <Header />
+        <main className={`${styles.main}`}>
+          <Routes>
+            <Route path="/" element={<Results sites={sites} />} />
+            <Route path="/create" element={<CreateForm />} />
+          </Routes>
+        </main>
+        <ToastContainer autoClose={3000} />
+      </div>
+    </Router>
   );
 }
 
@@ -42,10 +42,4 @@ const styles = {
   main: 'w-[90%] mx-auto grid grid-cols-3 gap-10',
 
   grid: 'grid grid-cols-3 gap-10',
-
-  sidebar: 'col-span-1',
-
-  results: 'col-span-2',
-
-  h2: 'pb-2 mb-6 border-b-2 border-b-dark/20 text-4xl font-bold uppercase tracking-widest',
 };
