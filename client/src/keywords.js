d90db34245fdc40e37f833a997558469426a0ffc
jxhnkndl <jkroyston@gmail.com>
2023-10-25 19:28:05 -0400

    split keywords into batch exports to create sections of related keywords on form ui

--- client/src/keywords.js
+++ client/src/keywords.js
@@ -1,58 +1,100 @@
-export const keywords = [
-  { keyword: 'Artist', name: 'type-artist'},
-  { keyword: 'Producer', name: 'type-producer'},
-  { keyword: 'Brand', name: 'type-brand'},
-  { keyword: 'Creator', name: 'type-creator'},
-  { keyword: 'Light Theme', name: 'theme-dark'},
-  { keyword: 'Dark Theme', name: 'theme-light'},
-  { keyword: 'Hybrid Theme', name: 'theme-hybrid'},
-  { keyword: 'Neutral Palette', name: 'palette-neutral'},
-  { keyword: 'Colorful Palette', name: 'palette-colorful'},
-  { keyword: 'Brand Color', name: 'brand-color'},
-  { keyword: 'Sans Fonts', name: 'fonts-sans'},
-  { keyword: 'Serif Fonts', name: 'fonts-serif'},
-  { keyword: 'Hybrid Fonts', name: 'fonts-hybrid'},
-  { keyword: 'SVG Logo', name: 'logo-svg'},
-  { keyword: 'HTML Text Logo', name: 'logo-html'},
-  { keyword: 'Wordmark', name: 'logo-wordmark'},
-  { keyword: 'Brandmark', name: 'logo-brandmark'},
-  { keyword: 'Micro Animations', name: 'animations-micro'},
-  { keyword: 'Big Animations', name: 'animations-big'},
-  { keyword: 'Scroll Reveal', name: 'animations-scroll'},
-  { keyword: 'Parallax', name: 'animations-parallax'},
-  { keyword: 'Single Page', name: 'pages-spa'},
-  { keyword: 'Multi Page', name: 'pages-mpa'},
-  { keyword: 'Open Nav', name: 'nav-open'},
-  { keyword: 'Closed Nav', name: 'nav-closed'},
-  { keyword: 'Hybrid Nav', name: 'nav-hybrid'},
-  { keyword: 'No Nav', name: 'nav-none'},
-  { keyword: 'CTA Listen', name: 'cta-listen'},
-  { keyword: 'CTA Buy', name: 'cta-buy'},
-  { keyword: 'CTA Sign Up', name: 'cta-sign-up'},
-  { keyword: 'CTA Contact', name: 'cta-contact'},
-  { keyword: 'CTA Other', name: 'cta-other'},
-  { keyword: 'No CTA', name: 'cta-none'},
-  { keyword: 'Video Hero', name: 'hero-video'},
-  { keyword: 'Photo Hero', name: 'hero-photo'},
-  { keyword: 'Abstract Hero', name: 'hero-abstract'},
-  { keyword: 'Other Hero', name: 'hero-other'},
-  { keyword: 'Tour Dates', name: 'section-tour'},
-  { keyword: 'Music', name: 'section-music'},
-  { keyword: 'Video', name: 'section-video'},
-  { keyword: 'About', name: 'section-about'},
-  { keyword: 'Photos', name: 'section-photos'},
-  { keyword: 'Other Section', name: 'section-other'},
-  { keyword: 'Internal Store', name: 'store-internal'},
-  { keyword: 'External Store', name: 'store-external'},
-  { keyword: 'Contact Form', name: 'contact-form'},
-  { keyword: 'Mailing List', name: 'mailing-list'},
-  { keyword: 'No Direct Contact', name: 'contact-missing'},
-  { keyword: 'Minimal', name: 'style-minimal'},
-  { keyword: 'Busy', name: 'style-busy'},
-  { keyword: 'Clean', name: 'style-clean'},
-  { keyword: 'Chaotic', name: 'style-chaotic'},
-  { keyword: 'Bright', name: 'style-bright'},
-  { keyword: 'Moody', name: 'style-moody'},
-  { keyword: 'High Quality', name: 'quality-high'},
-  { keyword: 'Low Quality', name: 'quality-low'},
-]
+export const categories = [
+  { keyword: 'Artist', name: 'type-artist' },
+  { keyword: 'Producer', name: 'type-producer' },
+  { keyword: 'Brand', name: 'type-brand' },
+  { keyword: 'Creator', name: 'type-creator' },
+];
+
+export const themes = [
+  { keyword: 'Light Theme', name: 'theme-dark' },
+  { keyword: 'Dark Theme', name: 'theme-light' },
+  { keyword: 'Hybrid Theme', name: 'theme-hybrid' },
+];
+
+export const colors = [
+  { keyword: 'Neutral Palette', name: 'palette-neutral' },
+  { keyword: 'Colorful Palette', name: 'palette-colorful' },
+  { keyword: 'Brand Color', name: 'brand-color' },
+];
+
+export const fonts = [
+  { keyword: 'Sans Fonts', name: 'fonts-sans' },
+  { keyword: 'Serif Fonts', name: 'fonts-serif' },
+  { keyword: 'Hybrid Fonts', name: 'fonts-hybrid' },
+];
+
+export const logoType = [
+  { keyword: 'SVG Logo', name: 'logo-svg' },
+  { keyword: 'HTML Text Logo', name: 'logo-html' },
+  { keyword: 'Wordmark', name: 'logo-wordmark' },
+  { keyword: 'Brandmark', name: 'logo-brandmark' },
+];
+
+export const animations = [
+  { keyword: 'Micro Animations', name: 'animations-micro' },
+  { keyword: 'Big Animations', name: 'animations-big' },
+  { keyword: 'Scroll Reveal', name: 'animations-scroll' },
+  { keyword: 'Parallax', name: 'animations-parallax' },
+];
+
+export const pages = [
+  { keyword: 'Single Page', name: 'pages-spa' },
+  { keyword: 'Multi Page', name: 'pages-mpa' },
+];
+
+export const navType = [
+  { keyword: 'Open Nav', name: 'nav-open' },
+  { keyword: 'Closed Nav', name: 'nav-closed' },
+  { keyword: 'Hybrid Nav', name: 'nav-hybrid' },
+  { keyword: 'No Nav', name: 'nav-none' },
+];
+
+export const ctas = [
+  { keyword: 'CTA Listen', name: 'cta-listen' },
+  { keyword: 'CTA Buy', name: 'cta-buy' },
+  { keyword: 'CTA Sign Up', name: 'cta-sign-up' },
+  { keyword: 'CTA Contact', name: 'cta-contact' },
+  { keyword: 'CTA Other', name: 'cta-other' },
+  { keyword: 'No CTA', name: 'cta-none' },
+];
+
+export const heroType = [
+  { keyword: 'Video Hero', name: 'hero-video' },
+  { keyword: 'Photo Hero', name: 'hero-photo' },
+  { keyword: 'Abstract Hero', name: 'hero-abstract' },
+  { keyword: 'Other Hero', name: 'hero-other' },
+];
+
+export const sections = [
+  { keyword: 'Tour Dates', name: 'section-tour' },
+  { keyword: 'Music', name: 'section-music' },
+  { keyword: 'Video', name: 'section-video' },
+  { keyword: 'About', name: 'section-about' },
+  { keyword: 'Photos', name: 'section-photos' },
+  { keyword: 'Other Section', name: 'section-other' },
+  { keyword: 'Internal Store', name: 'store-internal' },
+  { keyword: 'External Store', name: 'store-external' },
+];
+
+export const contactOptions = [
+  { keyword: 'Contact Form', name: 'contact-form' },
+  { keyword: 'Email', name: 'contact-email'},
+  { keyword: 'Mailing List', name: 'mailing-list' },
+  { keyword: 'No Direct Contact', name: 'contact-missing' },
+];
+
+export const siteStyles = [
+  { keyword: 'Minimal', name: 'style-minimal' },
+  { keyword: 'Busy', name: 'style-busy' },
+  { keyword: 'Clean', name: 'style-clean' },
+  { keyword: 'Chaotic', name: 'style-chaotic' },
+  { keyword: 'Bright', name: 'style-bright' },
+  { keyword: 'Moody', name: 'style-moody' },
+  { keyword: 'Responsive', name: 'style-responsive'},
+  { keyword: 'Not Responsive', name: 'style-not-responsive'}
+];
+
+export const quality = [
+  { keyword: 'High Quality', name: 'quality-high' },
+  { keyword: 'Low Quality', name: 'quality-low' },
+];
