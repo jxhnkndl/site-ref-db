5d7f5789f79a3095ee0e0805529b3cd93a4629ed
jxhnkndl <jkroyston@gmail.com>
2023-10-25 09:38:23 -0400

    start to build overall page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
-function App() {
+import CreateForm from "./components/CreateForm";
+import Header from "./components/Header";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App() {
   return (
-    <div>
-      <h1 className='text-8xl'>WEBREF</h1>
+    <div className={`${styles.pageContainer}`}>
+      <Header />
+      <main className={`${styles.main}`}>
+        <aside className={`${styles.sidebar}`}>
+          <CreateForm />
+        </aside>
+        <section className={`${styles.results}`}>
+          <h2 className={`${styles.h2}`}>Results</h2>
+        </section>
+      </main>
+      <ToastContainer autoClose={3000} />
     </div>
   );
 }
 
-export default App;
+const styles = {
+  pageContainer: 'min-h-screen bg-light',
+
+  main: 'w-[90%] mx-auto grid grid-cols-3 gap-10',
+
+  grid: 'grid grid-cols-3 gap-10',
+
+  sidebar: 'col-span-1',
+
+  results: 'col-span-2',
+
+  h2: 'pb-2 mb-6 border-b-2 border-b-dark/20 text-4xl font-bold uppercase tracking-widest',
+}
