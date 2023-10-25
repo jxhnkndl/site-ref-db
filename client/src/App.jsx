fa7d7b721a94b5390be076c6ccc03fe79ce0a962
jxhnkndl <jkroyston@gmail.com>
2023-10-25 12:03:18 -0400

    fetch sites on page load and render site components for each result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import CreateForm from "./components/CreateForm";
-import Header from "./components/Header";
+import { useState, useEffect } from 'react';
+
+import Header from './components/Header';
+import CreateForm from './components/CreateForm';
+import Site from './components/Site';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
+  const [sites, setSites] = useState([]);
+
+  useEffect(() => {
+    const fetchSites = async () => {
+      const res = await fetch('/api/sites');
+      const data = await res.json();
+      setSites(data);
+    };
+
+    fetchSites();
+  }, []);
+
   return (
     <div className={`${styles.pageContainer}`}>
       <Header />
@@ -13,6 +28,7 @@ export default function App() {
         </aside>
         <section className={`${styles.results}`}>
           <h2 className={`${styles.h2}`}>Results</h2>
+          {sites && sites.map((site) => <Site key={site._id} site={site} />)}
         </section>
       </main>
       <ToastContainer autoClose={3000} />
@@ -32,4 +48,4 @@ const styles = {
   results: 'col-span-2',
 
   h2: 'pb-2 mb-6 border-b-2 border-b-dark/20 text-4xl font-bold uppercase tracking-widest',
-}
+};
