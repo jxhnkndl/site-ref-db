bdc99b5726837afb3762a8b093d624d8ba9b8342
jxhnkndl <jkroyston@gmail.com>
2023-10-25 16:05:06 -0400

    add nav link for accessing create form route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import { IoIosAddCircle } from 'react-icons/io';
+
 export default function Header() {
   return (
     <header className={`${styles.header}`}>
       <div className={`${styles.container}`}>
-        <h1 className={`${styles.h1}`}>
-          <span className={`${styles.echo}`}>echo</span>DB
-        </h1>
+        <Link className={`${styles.brandLink}`} to='/'>
+          <h1 className={`${styles.h1}`}>
+            <span className={`${styles.echo}`}>echo</span>DB
+          </h1>
+        </Link>
+        <Link className={`${styles.navLink}`} to='/create'>
+          Add Site
+          <IoIosAddCircle className={`${styles.icon}`} />
+        </Link>
       </div>
     </header>
   );
@@ -15,7 +25,14 @@ const styles = {
 
   container: 'w-[90%] mx-auto flex justify-between items-center',
 
+  brandLink: 'hover:opacity-80 ease-out duration-[150ms]',
+
   echo: 'font-normal',
 
   h1: 'font-bold',
+
+  navLink:
+    'flex justify-center items-center text-base hover:opacity-80 ease-out duration-[150ms]',
+
+  icon: 'inline ml-1 text-light',
 };
