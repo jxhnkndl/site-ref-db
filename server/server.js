4f17bf1d60516cc1a206c9821ecb444acf1f2632
jxhnkndl <jkroyston@gmail.com>
2023-10-25 10:24:33 -0400

    small maintenance tweaks

--- server/server.js
+++ server/server.js
@@ -18,5 +18,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
 });
